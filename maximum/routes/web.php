443a26aa50d2c20d98c45a946dd78b01e6e5fe31
evilgar0503 <eduardovillar194@gmail.com>
2023-05-23 20:27:54 +0200

    authmiddleware

--- maximum/routes/web.php
+++ maximum/routes/web.php
@@ -58,9 +58,9 @@ Route::post('/cart/add', [CartController::class, 'add'])->name('cart.store');
 Route::post('/cart/update', [CartController::class, 'update'])->name('cart.update');
 Route::post('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
 Route::post('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
-Route::get('/checkout/{volver}', [CartController::class, 'checkout'])->name('checkout');
-Route::post('/checkout/review', [PedidoController::class, 'review'])->name('checkout.review');
-Route::post('/order', [PedidoController::class, 'store'])->name('order.complete');
+Route::get('/checkout/{volver}', [CartController::class, 'checkout'])->middleware('auth')->name('checkout');
+Route::post('/checkout/review', [PedidoController::class, 'review'])->middleware('auth')->name('checkout.review');
+Route::post('/order', [PedidoController::class, 'store'])->middleware('auth')->name('order.complete');
 
 Route::get('/order/{numero_seguimiento}', [RedirectionController::class, 'orderComplete'])->name('order');
 Route::get('/config/orders}', [RedirectionController::class, 'myOrders'])->name('myOrders');
